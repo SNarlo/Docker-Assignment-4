60ddf100d7eaef7eb65cf57ced78ef2a27d417af
Sebastian <sebastian.narloch82@gmail.com>
2021-10-20 21:30:08 +1000

    added hot-reloading to compose files

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI;
+const connectionString = process.env.MONGODB_DOCKER;
 
 mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
   console.error('Connection error', e.message);
