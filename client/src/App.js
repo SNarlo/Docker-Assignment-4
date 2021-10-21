c482b0183105a61c946add87953281d66d389a03
Sebastian <sebastian.narloch82@gmail.com>
2021-10-21 21:56:07 +1000

    started building the booking form with react-bootstrap elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import BookingForm from './components/forms/date-and-time-form';
 
 function App() {
   return (
     <div className="App">
-  
+      <BookingForm/>
     </div>
   );
 }
