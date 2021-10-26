95c89bcbab72ce5b6cf6737df9b986806c533a50
Sebastian <sebastian.narloch82@gmail.com>
2021-10-26 20:55:12 +1000

    Working on the booking form

--- client/src/components/forms/BookingForm.js
+++ client/src/components/forms/BookingForm.js
@@ -1,8 +1,10 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { ToggleButton } from 'react-bootstrap'
 import { ButtonGroup } from 'react-bootstrap'
 import './BookingForm.css' 
-import { Card, Button, Form } from 'react-bootstrap'
+import { Card, Button, Form} from 'react-bootstrap'
+// import bookingApis from '../../apis/bookings'
+import {useAuth} from '../../contexts/AuthContext'
 
 const BookingForm = () => {
     
@@ -18,16 +20,41 @@ const BookingForm = () => {
         { name : '18:00', value: '9'},
     ]
 
+    useEffect(() => {
+        userApis.getUser(currentUser.uid)
+        .then(user => {
+            setFirstName(user.FirstName)
+            setLastName(user.LastName)
+        })
+        .catch(err => console.log(err))
+    }, [currentUser.uid])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const newBooking = {
+            FirstName : currentUser.FirstName,
+            LastName : currentUser.LastName,
+            BookingWith : staffRef.current.value,
+            BookingDate : dateRef.current.value,
+            BookingTime : times.name
+        }
+
+        console.log(newBooking)
+    }
+
+    const {currentUser} = useAuth()
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
     const dateRef = useRef()
     // const timeRef = useRef()
-    // const [checked, setChecked] = useState(false)
+    const staffRef = useRef()
     const [timeValue, setTimeValue] = useState(0)
     
-
     return (
         <div className='booking-form-container'>
-                <Form className='booking-form-body'>
+                <Form  onSubmit={handleSubmit} className='booking-form-body'>
                     <h1 className='form-title'>make a booking</h1>
                     <Card className='card' id='calendar-card'>
                         <Form.Group>
@@ -37,26 +64,36 @@ const BookingForm = () => {
                     </Card>
                     <h1 className='form-title'>select a time</h1>
                     <Card className='card' id='time-slot-card'>
-                       <Card.Body className='times'>
-                            <ButtonGroup>
-                              {times.map((time, key) => (
-                                  <ToggleButton
-                                    key={key}
-                                    id={`time-${key}`}
-                                    type='radio'
-                                    name='type'
-                                    className='time-value'
-                                    value={time.value}
-                                    checked={timeValue === time.value}
-                                    onChange={(e) => setTimeValue(e.currentTarget.value)}
-                                  >
-                                    {time.name}
-                                  </ToggleButton>
-                            ))}
-                            </ButtonGroup>
-                       </Card.Body>
-                    <Button>Make a Booking!</Button>
-                    {/* need a POST event on submit */}
+                        <Card.Body className='times'>
+                           <Form.Check required='true'>
+                                <ButtonGroup>
+                                    {times.map((time, key) => (
+                                        <ToggleButton 
+                                            key={key}
+                                            id={`time-${key}`}
+                                            type='radio'
+                                            name='type'
+                                            className='time-value'
+                                            value={time.value}
+                                            checked={timeValue === time.value}
+                                            onChange={(e) => setTimeValue(e.currentTarget.value)}
+                                        >
+                                            {time.name}
+                                        </ToggleButton>
+                                    ))}
+                                </ButtonGroup>
+                           </Form.Check>
+                            <Form.Group>
+                                <Form.Label>Who would you like you booking with?</Form.Label>
+                                <Form.Control as='select' ref={staffRef}>
+                                    <option>Select a staff member</option>
+                                    <option value="Tom">Tom</option>
+                                    <option value="Sarah">Sarah</option>
+                                    <option value="James">James</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Card.Body>
+                        <Button type='Submit'>Make a Booking!</Button>
                     </Card>
                 </Form>
         </div>
