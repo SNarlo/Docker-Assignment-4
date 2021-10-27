303d0129d796403bc7c65dd47680a32042ad6cf3
Sebastian <sebastian.narloch82@gmail.com>
2021-10-27 21:36:39 +1000

    added header to thank you message component to get rid of logging out issue

--- client/src/components/pages/ThankYouPage.js
+++ client/src/components/pages/ThankYouPage.js
@@ -1,28 +1,15 @@
 import './ThankYouPage.css'
+import React from 'react'
 import { Card, Button } from 'react-bootstrap'
 import Logo from '../../assets/brick-&-mortar-logo.png'
-import { useAuth } from '../../contexts/AuthContext'
-import { useHistory } from 'react-router-dom'
+import Header from '../header/Header'
 
 const ThankYouPage = (props) => {
 
-    const { logOut } = useAuth() 
-    const history = useHistory()
- 
-    // Logs the user out of the booking app
-    const handleLogout = async () => {
-        try {
-            await logOut()
-            history.push('/login')
-        } catch(err) {
-            console.log(err)
-        }
-    }
-
     return (
         <div>
+            <Header />
             <Card>
-                <img id='thank-you-logo' src={Logo} alt='brick & mortar logo'></img>
                 <Card.Body>
                     <p>Thank you for making a booking with the brick & mortar store. Your booking details are below: </p>
                     <h2>Booking for: {props.location.state.firstName} {props.location.state.lastName}</h2>
@@ -30,7 +17,7 @@ const ThankYouPage = (props) => {
                     <h2>Booking Time: {props.location.state.time}</h2>
                     <h2>With: {props.location.state.staff}</h2>
                 </Card.Body>
-                <Button onClick={() => handleLogout}>Sign Out</Button>
+                <Button>Sign Out</Button>
             </Card>
         </div>
     )
